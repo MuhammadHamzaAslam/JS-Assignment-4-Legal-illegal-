ff47212c63c7910a3a7a3a3111b552b2d5d52684
MuhammadHamzaAslam <hamzaaslam19087@gmail.com>
2024-02-29 22:15:26 +0500

    commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var x ="Muhammad",y="Hamza",z="Aslam"
+console.log(x+y+z);
+var heading = "<h1>Rules for naming JS variables</h1>"
+document.write(heading)
+var para1 = "Variable names can only contain numbers  $ and _. For example:$my_1stVariable"
+document.write(para1)
+var para2 = " <br> <br> Variable  Must Begin with letter ,$ , or_.For example:$name name or name"
+document.write(para2)
+var para3 = " <br> <br> Variable name are case sensitive"
+document.write(para3)
+var para4 = " <br> <br> Variable name should not be JS keyword"
+document.write(para4)
